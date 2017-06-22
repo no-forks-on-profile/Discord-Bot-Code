99199d858883de985a44546859cbfa1ad8674a48
Gitcodesoftware <RyanMcKeown82@Gmail.com>
2017-06-22 20:49:15 +0800

    Update app.js
    Made help command layout look neater and easier to read, wew.

--- app.js
+++ app.js
@@ -123,7 +123,30 @@ if (message.content.startsWith(config.prefix + 'roll')) {
 if (message.content.startsWith(config.prefix + 'help')) {
   message.react('👌');
   message.channel.send('You\'ve been DMed a list of commands! :mailbox_with_mail:')
-  message.author.send(`__<botname> Commands__\n\n__Fun__\n**ping**: Checks if the bot\'s still alive\n**roll**: Rolls a random number!\n**rate**: Will rate the user who said it\n**8ball**: The bot will respond to any question you throw at it! (This is, of course, with the set responses)\n**flipcoin**: The bot will respond with either heads or tails!\n**say**: Will say the users input!\n**avatar**: Displays the users avatar! (Works with GIFs too!)\n**lenny**: Will create a lenny face!\n**tableflip**: The bot will throw tables!\n**unflip**: Unflips a table!\n**f**: Pay your respects\n\n__Moderation__\n**purge**: The bot will purge any message (Limit: 2 - 100 Messages)\n**ban**: Bans the user specified\n**kick**: Kicks the user specified\n**mute**: Mutes the user specified\n**unmute**: Unmutes the user specified\n\n__Utility__\n**support**: Directs you to the support server!\n**invite**: Gives bot invite link, this is useful for adding the bot to your server!\n**uptime**: Shows bot uptime`)
+  message.author.send(`__<botName> Commands__
+    \n\n__Fun__
+    \n**ping**: Checks if the bot\'s still alive
+    \n**roll**: Rolls a random number!
+    \n**rate**: Will rate the user who said it
+    \n**8ball**: The bot will respond to any question you throw at it! (This is, of course, with the set responses)
+    \n**flipcoin**: The bot will respond with either heads or tails!
+    \n**say**: Will say the users input!
+    \n**avatar**: Displays the users avatar! (Works with GIFs too!)
+    \n**lenny**: Will create a lenny face!
+    \n**tableflip**: The bot will throw tables!
+    \n**unflip**: Unflips a table!
+    \n**f**: Pay your respects
+    \n\n__Moderation__
+    \n**purge**: The bot will purge any message (Limit: 2 - 100 Messages)
+    \n**ban**: Bans the user specified
+    \n**kick**: Kicks the user specified
+    \n**mute**: Mutes the user specified
+    \n**unmute**: Unmutes the user specified
+    \n\n__Utility__
+    \n**support**: Directs you to the support server!
+    \n**invite**: Gives bot invite link, this is useful for adding the bot to your server!
+    \n**uptime**: Shows bot uptime
+    \n**serverinfo**: Shows info about the server`)
 }
 
 if (message.content.startsWith(config.prefix + 'avatar')) {
